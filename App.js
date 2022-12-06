5a7b97e3fab6efc77f80d8e6573df395778e07a7
pong7772 <roth.samnangvisothipong19@kit.edu.kh>
2022-12-06 14:08:13 +0700

    update

--- App.js
+++ App.js
@@ -3,7 +3,18 @@ import { StyleSheet, Image, Text, View, FlatList } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 import RootNavigator from './src/navigation/navigation';
-export default function App() {
+// aws amplify
+import { Amplify } from 'aws-amplify'
+import config from './src/aws-exports'
+//authentication
+import { withAuthenticator } from 'aws-amplify-react-native';
+// we need to connect amplify and config together
+
+
+//configure project connect with our backend
+Amplify.configure(config)
+
+function App() {
 
   return (
     <NavigationContainer>
@@ -12,3 +23,4 @@ export default function App() {
     </NavigationContainer>
   );
 }
+export default withAuthenticator(App);
