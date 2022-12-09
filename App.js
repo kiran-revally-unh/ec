411775467b1912cfc771bd58cc8d623a64924511
pong7772 <roth.samnangvisothipong19@kit.edu.kh>
2022-12-09 11:20:58 +0700

    test

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Image, Text, View, FlatList } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 import RootNavigator from './src/navigation/navigation';
@@ -7,11 +6,13 @@ import RootNavigator from './src/navigation/navigation';
 import { Amplify } from 'aws-amplify'
 import config from './src/aws-exports'
 //authentication
+//this is we can check with the link https://docs.amplify.aws/start/q/integration/react-native/
 import { withAuthenticator } from 'aws-amplify-react-native';
 // we need to connect amplify and config together
 
 
 //configure project connect with our backend
+// we add only this config
 Amplify.configure(config)
 
 function App() {
