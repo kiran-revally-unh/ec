feb75c6c831aa02a26f356f7a66d4822fbb3c6cb
pong7772 <roth.samnangvisothipong19@kit.edu.kh>
2023-01-09 14:40:28 +0700

    update on backend

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,9 +1,21 @@
 
 import { StyleSheet, Image, Text, View, FlatList } from 'react-native';
 import RestaurantItem from "../../components/RestaurantItem/item";
-import restaurants from "../../../assets/data/restaurants.json"
+import { useState, useEffect } from 'react';
+import { DataStore } from 'aws-amplify';
+import { Restuarant } from '../../models';
+// import restaurants from "../../../assets/data/restaurants.json"
 export default function HomeScreen() {
-
+    const [restaurants, setRestaurants] = useState([])
+    //long line
+    // const fetchRestaurants = async () => {
+    //     const results = await DataStore.query(Restuarant)
+    //     setRestaurants(results)
+    // }
+    useEffect(() => {
+        //do anything
+        DataStore.query(Restuarant).then((results) => setRestaurants(results))
+    }, [])
     return (
         <View style={styles.page}>
             {/* container restaurant item */}
--- src/screens/RetaurantDetailsScreen/RestaurantDetails.js
+++ src/screens/RetaurantDetailsScreen/RestaurantDetails.js
@@ -1,15 +1,29 @@
 import React from 'react';
-import { View, StyleSheet, Text, Image, FlatList, Pressable } from 'react-native';
-import restaurants from '../../../assets/data/restaurants.json'
+import { View, StyleSheet, Text, Image, FlatList, Pressable, ActivityIndicator } from 'react-native';
+// import restaurants from '../../../assets/data/restaurants.json'
 import DishListItem from '../../components/DishListItem/dishListItem';
 import RestaurantHeader from './RestaurantHeader';
 import { AntDesign } from '@expo/vector-icons';
 import { useRoute, useNavigation } from '@react-navigation/native';
+import { useState, useEffect } from 'react';
+import { DataStore } from 'aws-amplify';
+import { Restuarant } from '../../models';
 const RestaurantDetails = () => {
-    const restaurant = restaurants[0]
+    // const restaurant = restaurants[0]
+    const [restaurant, setRestaurant] = useState(null)
     const route = useRoute()
     const id = route.params?.id;
+    const fetchRestaurant = () => {
+        DataStore.query(Restuarant, id).then(setRestaurant)
+    }
+    useEffect(() => {
+        //fetch the restaurant with id
+        fetchRestaurant()
+    }, [])
     const navigation = useNavigation()
+    if (!restaurant) {
+        return <ActivityIndicator size={'large'} color='gray' style={{ alignItems: 'center', justifyContent: 'center', marginTop: 50, paddingVertical: 30 }} />
+    }
     return (
         <View style={styles.page}>
             <FlatList data={restaurant.dishes}
