21c592168024a7bcd5ce57fb109c84f6d0f57788
pong7772 <roth.samnangvisothipong19@kit.edu.kh>
2023-01-16 13:46:11 +0700

    update basket

--- src/contexts/BasketContext.js
+++ src/contexts/BasketContext.js
@@ -1,14 +1,25 @@
 import { createContext, useState, useEffect, useContext } from "react";
 import { DataStore } from "aws-amplify";
 import { Basket, BasketDish } from "../models";
+import { useAuthContext } from "./AuthContext";
 const BasketContext = createContext({});
 
 const BasketContextProvider = ({ children }) => {
+    const [basket, setBasket] = useState(null)
+    const [restaurant, setRestaurant] = useState(null)
+    const { sub } = useAuthContext()
+
+
     const addDishToBasket = (dish, quantity) => {
-        console.log(dish, quantity)
+        // get the existing busket or create a new one 
+
+
+        //create a basketdish item and save to datastore
+
+
     }
     return (
-        <BasketContext.Provider value={{ addDishToBasket }}>
+        <BasketContext.Provider value={{ addDishToBasket, setRestaurant }}>
             {children}
         </BasketContext.Provider>
     )
